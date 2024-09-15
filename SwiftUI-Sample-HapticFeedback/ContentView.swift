e4b319d0f7e8e1943c213982013c7d3fc9fc5634
NanbanTaro <>
2024-09-15 12:27:31 +0900

    Create New Project

---
+++ SwiftUI-Sample-HapticFeedback/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sample-HapticFeedback
+//
+//  Created by NanbanTaro on 2024/09/15.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Sample-HapticFeedback/SwiftUI_Sample_HapticFeedbackApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_Sample_HapticFeedbackApp.swift
+//  SwiftUI-Sample-HapticFeedback
+//
+//  Created by NanbanTaro on 2024/09/15.
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_HapticFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
